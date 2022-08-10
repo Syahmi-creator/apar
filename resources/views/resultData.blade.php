745967aa26d83551c31512c952eff05006a3654a
adlina6 <adlina6@graduate.utm.my>
2022-08-10 12:13:19 +0800

    Fix the excelView detect database masuk ke tak

--- resources/views/resultData.blade.php
+++ resources/views/resultData.blade.php
@@ -36,7 +36,7 @@
                     @foreach ($courses as $course1)
                         @if($course1->students->where('user_id', Auth()->id())->count() > 0)
                         <tr>
-                            // <td class="border text-center">{{ $loop->iteration }}</td>
+                           {{-- <td class="border text-center">{{ $loop->iteration }}</td> --}}
                             <td class="border text-center">{{ $i++ }}</td>
                             <td class="border text-center">{{ $course1->year_offer }}</td>
                             <td class="border text-center">{{ $course1->semester_offer }}</td>
