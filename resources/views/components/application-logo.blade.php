df1ed083067b785c177e792e444e46e6d08cf520
adlina6 <adlina6@graduate.utm.my>
2022-08-15 13:37:26 +0800

    settings up in the storage UTM Logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
+
 {{-- <img src="{{ /UTM-LOGO.png) }}" alt="utm_logo" width="50" height="50"> --}}
-=======
+
 <img src="{{ asset('/storage/UTM-LOGO.png') }}" alt="utm_logo" width="50" height="50">
->>>>>>> a0e92ad497348473ca5304f54d35e7289ad464fd
+
