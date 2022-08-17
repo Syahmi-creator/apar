181d96c4f7541f0133e55dbdfb2b1e763b5331b2
adlina6 <adlina6@graduate.utm.my>
2022-08-17 18:02:21 +0800

    updated the KPI Passed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,8 +4,8 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 @forelse ($images as $image)
@@ -18,5 +18,52 @@
                 @endforelse
             </div>
         </div>
+    </div> --}}
+
+  <!-- Content Row -->
+  <div class="row">
+    <div class="col-sm-12">
+    <div class="card text-center">
+        <div class="card-header">
+          Information for APAR cohort
+        </div>
+        <div class="card-body">
+            <form action="{{ route('cohort.store') }}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="graduation_session">Graduation Session:</label>
+                    <select class="form-control" name="graduation_session" aria-label="Select Graduation Session" required>
+                        <option value="2021/2022">2021/2022</option>
+                        <option value="2022/2023">2022/2023</option>
+                        <option value="2023/2024">2023/2024</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="graduation_semester">Graduation Semester:</label>
+                    <select class="form-control" name="graduation_semester" aria-label="Select Graduation Semester" required>
+                        <option value="1">I</option>
+                        <option value="2">II</option>
+                    </select>
+                </div>
+
+
+                <div class="form-group">
+                    <label for="total_student">Total number of students for this cohort:</label>
+                    <input type="number" name="total_student" class="form-control" required>
+                </div>
+
+
+
+
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+
+            </form>
+        </div>
+      </div>
+
     </div>
+
+</div>
+
 </x-app-layout>
--- resources/views/resultDataPass.blade.php
+++ resources/views/resultDataPass.blade.php
@@ -33,11 +33,13 @@
                 </tr>
             </thead>
             <tbody>
-
+                @php
+                    $i = 1;
+                @endphp
                 @foreach ($courses as $course1)
                     @if ($course1->students->where('user_id', Auth()->id())->count() > 0)
                         <tr>
-                            <td class="border text-center">{{ 1 }}</td>
+                            <td class="border text-center">{{ $i++ }}</td>
                             {{-- <td class="border text-center">{{ $course1->year_offer }}</td>
                             <td class="border text-center">{{ $course1->semester_offer }}</td> --}}
                             <td class="border text-center">{{ $course1->course_name }}</td>
@@ -45,107 +47,120 @@
                             <td class="border text-center">{{ $course1->credit_hour }}</td>
                             <td class="border text-center">
                                 {{ $course1->students->where('user_id', Auth()->id())->count() }}</td>
-                            <td class="border text-center"> @php
+                            <td class="border text-center">
+                                @php
                                 $PO1 = 0;
-                                $T_studentPassedPO1 = $course1->students->where('user_id', Auth()->id())->where('PO1', '>', 0);
+                                $T_studentPassedPO1 = $course1->students->where('user_id', Auth()->id())->where('PO1', '>', 0)->count();
                                 foreach ($course1->students as $student) {
                                     if ($student->PO1 >= 65.0) {
                                         $PO1++;
                                     }
                                 }
+
+                                $resultPO1 = $PO1/$T_studentPassedPO1;
                             @endphp
-                                {{ $PO1 }}</td>
+                                {{ $resultPO1 }}</td>
                             <td class="border text-center"> @php
                                 $PO2 = 0;
-                                $T_studentPassedPO1 = $course1->students->where('user_id', Auth()->id())->where('PO2', '>', 0);
+                                $T_studentPassedPO2 = $course1->students->where('user_id', Auth()->id())->where('PO2', '>', 0)->count();
                                 foreach ($course1->students as $student) {
                                     if ($student->PO2 >= 65.0) {
-                                        $PO1++;
+                                        $PO2++;
                                     }
                                 }
+                                $resultPO2 = $PO2/$T_studentPassedPO2;
                             @endphp
-                                {{ $PO2 }}</td>
+                                {{ $resultPO2 }}</td>
+
                             <td class="border text-center"> @php
                                 $PO3 = 0;
-                                $T_studentPassedPO1 = $course1->students->where('user_id', Auth()->id())->where('PO3', '>', 0);
+                                $T_studentPassedPO3 = $course1->students->where('user_id', Auth()->id())->where('PO3', '>', 0)->count();
                                 foreach ($course1->students as $student) {
                                     if ($student->PO3 >= 65.0) {
                                         $PO3++;
                                     }
                                 }
+                                $resultPO3 = $PO3/$T_studentPassedPO3;
                             @endphp
-                                {{ $PO3 }}</td>
+                                {{ $resultPO3 }}</td>
                             </td>
                             <td class="border text-center">@php
                                 $PO4 = 0;
-                                $T_studentPassedPO1 = $course1->students->where('user_id', Auth()->id())->where('PO4', '>', 0);
+                                $T_studentPassedPO4 = $course1->students->where('user_id', Auth()->id())->where('PO4', '>', 0)->count();
                                 foreach ($course1->students as $student) {
                                     if ($student->PO4 >= 65.0) {
                                         $PO4++;
                                     }
                                 }
+                                $resultPO4 = $PO4/$T_studentPassedPO4;
                             @endphp
-                                {{ $PO4 }}</td>
+                                {{ $resultPO4 }}</td>
                             <td class="border text-center">@php
                                 $PO5 = 0;
-                                $T_studentPassedPO1 = $course1->students->where('user_id', Auth()->id())->where('PO5', '>', 0);
+                                $T_studentPassedPO5 = $course1->students->where('user_id', Auth()->id())->where('PO5', '>', 0)->count();
                                 foreach ($course1->students as $student) {
                                     if ($student->PO5 >= 65.0) {
                                         $PO5++;
                                     }
                                 }
+                                $resultPO5 = $PO5/$T_studentPassedPO5;
                             @endphp
-                                {{ $PO5 }}</td>
+                                {{ $resultPO5 }}</td>
                             <td class="border text-center">@php
                                 $PO6 = 0;
-                                $T_studentPassedPO1 = $course1->students->where('user_id', Auth()->id())->where('PO6', '>', 0);
+                                $T_studentPassedPO6 = $course1->students->where('user_id', Auth()->id())->where('PO6', '>', 0)->count();
                                 foreach ($course1->students as $student) {
                                     if ($student->PO6 >= 65.0) {
                                         $PO6++;
                                     }
                                 }
+                                $resultPO6 = $PO6/$T_studentPassedPO6;
                             @endphp
-                                {{ $PO6 }}</td>
+                                {{ $resultPO6 }}</td>
                             <td class="border text-center">@php
                                 $PO7 = 0;
-                                $T_studentPassedPO1 = $course1->students->where('user_id', Auth()->id())->where('PO7', '>', 0);
+                                $T_studentPassedPO7 = $course1->students->where('user_id', Auth()->id())->where('PO7', '>', 0)->count();
                                 foreach ($course1->students as $student) {
                                     if ($student->PO7 >= 65.0) {
                                         $PO7++;
                                     }
                                 }
+                                $resultPO7 = $PO7/$T_studentPassedPO7;
                             @endphp
-                                {{ $PO7 }}</td>
+                                {{ $resultPO7 }}</td>
                             <td class="border text-center">@php
                                 $PO8 = 0;
-                                $T_studentPassedPO1 = $course1->students->where('user_id', Auth()->id())->where('PO8', '>', 0);
+                                $T_studentPassedPO8 = $course1->students->where('user_id', Auth()->id())->where('PO8', '>', 0)->count();
                                 foreach ($course1->students as $student) {
                                     if ($student->PO8 >= 65.0) {
                                         $PO8++;
                                     }
                                 }
+                                $resultPO8 = $PO8/$T_studentPassedPO8;
                             @endphp
                                 {{ $PO8 }}</td>
                             <td class="border text-center">@php
                                 $PO9 = 0;
-                                $T_studentPassedPO1 = $course1->students->where('user_id', Auth()->id())->where('PO9', '>', 0);
+                                $T_studentPassedPO9 = $course1->students->where('user_id', Auth()->id())->where('PO9', '>', 0)->count();
                                 foreach ($course1->students as $student) {
                                     if ($student->PO9 >= 65.0) {
                                         $PO9++;
                                     }
                                 }
+                                $resultPO9 = $PO9/$T_studentPassedPO9;
                             @endphp
-                                {{ $PO9 }}</td>
+                                {{  $resultPO9 }}</td>
                             <td class="border text-center">@php
                                 $PO10 = 0;
-                                $T_studentPassedPO1 = $course1->students->where('user_id', Auth()->id())->where('PO10', '>', 0);
+                                $T_studentPassedPO10 = $course1->students->where('user_id', Auth()->id())->where('PO10', '>', 0)->count();
                                 foreach ($course1->students as $student) {
                                     if ($student->PO10 >= 65.0) {
                                         $PO10++;
                                     }
                                 }
+                                $resultPO10 = $PO10/$T_studentPassedPO10;
                             @endphp
-                                {{ $PO10 }}</td>
+                                {{  $resultPO10 }}</td>
                         </tr>
                     @endif
                 @endforeach
