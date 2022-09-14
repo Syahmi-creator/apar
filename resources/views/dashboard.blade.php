6a6338575f01e6e4c545fad96321b9a41942f2d7
adlina6 <adlina6@graduate.utm.my>
2022-09-14 22:36:20 +0800

    Test everything version 2022.0124

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,32 +6,16 @@
     </x-slot>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    {{-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                @forelse ($images as $image)
-                <div class="font-semibold text-2xl text-center p-6 bg-white border-b border-gray-200">
-                    <div class="pb-6">{{ substr($image->name, 0, strpos($image->name, ".")) }}</div>
-                    <img src="{{ asset($image->image_path) }}">
-                </div>
-                @empty
-                <span>Nothing uploaded.</span>
-                @endforelse
-            </div>
-        </div>
-    </div> --}}
 
-    <!-- Content Row -->
+
     <div class="container">
-
-       <h5> Only test user can see this dashboard </h5>
         <div class="row">
             <div class="col-sm-12">
                 <div class="card">
                     <div class="card-header">
                         Information for APAR cohort
                     </div>
-                </div>
+
                 <div class="card-body">
                     <form action="{{ route('dashboard.store') }}" method="POST">
                         @csrf
@@ -66,45 +50,9 @@
                 </div>
             </div>
         </div>
+        </div>
 
-        {{-- <p> Task 21/8/2022, updated all the user and password, the year of APAR still configured (for now need to deleted file to proceed with a new year of APAR) </p>
-        <table class="table table-responsive">
-            <thead>
-                <tr>
-                    <th class="border">No</th>
-                    <th class="border text-center">Email</th>
-                    <th class="border text-center">Password</th>
 
-                </tr>
-            </thead>
-            <tbody>
-                        <tr class="table-primary">
-                            <td class="border text-center">1</td>
-                            <td class="border text-center">secrh@gmail.com</td>
-                            <td class="border text-center">password</td>
-                          </tr>
-                          <tr class="table-secondary">
-                            <td class="border text-center">2</td>
-                            <td class="border text-center">secjh@gmail.com</td>
-                            <td class="border text-center">password</td>
-                          </tr>
-                          <tr class="table-success">
-                            <td class="border text-center">3</td>
-                            <td class="border text-center">secph@gmail.com</td>
-                            <td class="border text-center">password</td>
-                          </tr>
-                          <tr class="table-info">
-                            <td class="border text-center">4</td>
-                            <td class="border text-center">secvh@gmail.com</td>
-                            <td class="border text-center">password</td>
-                          </tr>
-                          <tr class="table-warning">
-                            <td class="border text-center">5</td>
-                            <td class="border text-center">secbh@gmail.com</td>
-                            <td class="border text-center">password</td>
-                          </tr>
-            </tbody>
-        </table> --}}
     </div>
 
 </x-app-layout>
