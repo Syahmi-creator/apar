8d96ab64c47fad7321b5a47ed3e83f0f28ecc4a7
adlina6 <adlina6@graduate.utm.my>
2022-09-20 11:13:01 +0800

    Test everything version 2022.0167

---
+++ database/migrations/2022_09_20_030202_create_studentfiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('studentfiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('file_path')->nullable();
+            $table->string('excel_path')->nullable();
+            $table->foreignId('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('studentfiles');
+    }
+}
