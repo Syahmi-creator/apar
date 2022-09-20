48981703ad310421c2e3aefaf822836a19dfb8aa
adlina6 <adlina6@graduate.utm.my>
2022-09-20 15:42:46 +0800

    Test everything version 2022.0168

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 
 {{-- <img src="{{ /UTM-LOGO.png) }}" alt="utm_logo" width="50" height="50"> --}}
 
-<img src="{{ asset('/storage/UTM-LOGO.png') }}" alt="utm_logo" width="50" height="50">
+{{-- <img src="{{ asset('/storage/UTM-LOGO.png') }}" alt="utm_logo" width="50" height="50"> --}}
 
