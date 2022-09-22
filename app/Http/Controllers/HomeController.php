375635f6411bb1678feb9300d64a1bd18d9a441f
adlina6 <adlina6@graduate.utm.my>
2022-09-22 11:04:41 +0800

    Test everything version 2022.0178

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,21 +47,21 @@ class HomeController extends Controller
 
     public function store(Request $req){
 
-        $req->validate([
-            'file' => 'required|mimes:xlx,xls|max:2048'
-            ]);
-            $fileModel = new File;
+        // $req->validate([
+        //     'file' => 'required|mimes:xlx,xls|max:2048'
+        //     ]);
+        //     $fileModel = new File;
             $user_id = Auth()->id();
-            if($req->file()) {
-                $fileName = time().'_'.$req->file->getClientOriginalName();
-                $formFile = $fileName;
-                $filePath = $req->file('file')->storeAs('uploads', $fileName, 'public');
-                $fileModel->name = time().'_'.$req->file->getClientOriginalName();
-                $fileModel->file_path = '/storage/' . $filePath;
-                $fileModel->save();
-                return back()
-                ->with('success','File has been uploaded.')
-                ->with('file', $fileName);
+            // if($req->file()) {
+            //     $fileName = time().'_'.$req->file->getClientOriginalName();
+            //     $formFile = $fileName;
+            //     $filePath = $req->file('file')->storeAs('uploads', $fileName, 'public');
+            //     $fileModel->name = time().'_'.$req->file->getClientOriginalName();
+            //     $fileModel->file_path = '/storage/' . $filePath;
+            //     $fileModel->save();
+            //     return back()
+            //     ->with('success','File has been uploaded.')
+            //     ->with('file', $fileName);
 
                 $informations = Information::create(
                     [
@@ -87,7 +87,7 @@ class HomeController extends Controller
 
 
             }
-        }
+
             public function fileImportExport()
             {
                return view('file-import');
